41c68f8e34a0633065c17fd1db1cf412d687562b
Marvin Pacis <108392678+marventures@users.noreply.github.com>
2024-03-07 20:55:32 +0800

    Update contactsSelector.js

--- src/redux/contacts/contactsSelector.js
+++ src/redux/contacts/contactsSelector.js
@@ -6,7 +6,9 @@ export const selectError = state => state.contacts.error;
 export const selectIsLoading = state => state.contacts.isLoading;
 
 export const selectFilteredContacts = createSelector(
+  // array of input selectors
   [selectContacts, selectFilter],
+  // transducer function
   (contacts, filter) => {
     return contacts.filter(({ name }) =>
       name.toLowerCase().includes(filter.toLowerCase())
